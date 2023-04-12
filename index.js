4450885061843b64a8f3970056f5757b7ab2b7a2
svetoslavmanolov <svn.manolov@gmail.com>
2023-04-12 19:03:26 +0300

    Third commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const hbs = require('express-handlebars');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const whitelist = ['http://localhost:3000']
+const whitelist = ['http://localhost:3000'];
 
 const routes = require('./routes');
 const { dbInit } = require('./config/db');
@@ -17,8 +17,14 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
+// app.engine('hbs', hbs.engine({
+//     extname: 'hbs'
+// }));
 
-app.use(cors({ origin: whitelist, credentials: true }));
+// app.set('view engine', 'hbs');
+
+// app.use(cors({ origin: whitelist, credentials: true }));
+app.use(cors({ origin: '*', credentials: true }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
